df8352fd0b37cb0590429166d004dbdeb200739f
Aswin <aswinravivarman@gmail.com>
2026-03-04 18:03:40 +0530

    Added password in user module

--- courseenrollment/src/main/java/com/example/courseenrollment/entity/User.java
+++ courseenrollment/src/main/java/com/example/courseenrollment/entity/User.java
@@ -25,6 +25,8 @@ public class User {
     @Column (nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private String password;
 
     @Enumerated (EnumType.STRING)
     private Role role;
@@ -44,6 +46,14 @@ public class User {
 
     public void setName(String username) {
         this.name = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Role getRole() {

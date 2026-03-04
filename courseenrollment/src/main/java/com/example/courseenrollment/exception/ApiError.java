c24cc00e516abeb02ab9f14e28d1f83b12bbea5d
Kavya <2k22cse077@kiot.ac.in>
2026-03-04 19:54:04 +0530

    Rectified the errors

--- courseenrollment/src/main/java/com/example/courseenrollment/exception/ApiError.java
+++ courseenrollment/src/main/java/com/example/courseenrollment/exception/ApiError.java
@@ -2,11 +2,13 @@ package com.example.courseenrollment.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ApiError {
 
     private LocalDateTime timestamp;

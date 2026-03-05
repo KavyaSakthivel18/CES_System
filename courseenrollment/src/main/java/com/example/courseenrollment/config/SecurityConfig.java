ff06cd763d2169058698577ff9079b56069ed037
KavyaSakthivel18 <2k22cse077@kiot.ac.in>
2026-03-05 12:31:37 +0530

    Update CORS allowed origins for production

--- courseenrollment/src/main/java/com/example/courseenrollment/config/SecurityConfig.java
+++ courseenrollment/src/main/java/com/example/courseenrollment/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://ces-system.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
